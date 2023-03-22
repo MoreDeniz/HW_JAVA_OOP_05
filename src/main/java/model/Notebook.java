e44212c628e4aaa382d1db002e1d07ea516ef50f
Mayya <msalosina@mail.ru>
2023-03-22 19:49:44 +0700

    model.Notebook

--- src/main/java/model/Notebook.java
+++ src/main/java/model/Notebook.java
@@ -1,73 +1,30 @@
-package notebook;
+package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import presenter.Presenter;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+public class Notebook implements Model{
+    List<String> entries = new ArrayList<>();
+    private String text;
 
-public class Notebook extends Note{
+    public void addEntry(String text){
+        entries.add(text);
 
-    private String note;
-    private Scanner scanner;
-    private Presenter presenter;
-    private static StringBuilder sb;
-
-    public void setPresenter(Presenter presenter){
-        this.presenter = presenter;
     }
-
-    public Notebook() throws Exception{
-        scanner = new Scanner(System.in);
-        this.sb = new StringBuilder();
-        readFile();
+    @Override
+    public String toString(){
+        return String.join("\n", entries);
     }
-
-    public void menu(int number) throws Exception {
-        start(number);
-    }
-
-    public void start(int number) throws Exception {
-        if (number == 1){
-            addNote();
-        }
-        if (number == 2){
-            readNotes();
+    public void showEntry(){
+        for (String e : entries){
+            System.out.println(e);
         }
     }
 
-    private void addNote() throws Exception {
-        System.out.println("Введите текст: \n");
-        note = scanner.nextLine();
-        sb.append(note + "\n");
-        readFile();
-        back();
-    }
-
-    private void readNotes() throws IOException{
-        System.out.println(sb.toString());
-        back();
-    }
-
-    private void back() throws IOException{
-        System.out.println(sb.toString());
-        back();
-    }
-
-    public void readFile() throws Exception{
-        FileReader fr = new FileReader("notebook.txt");
-        Scanner scan = new Scanner(fr);
-        while (scan.hasNextLine()){
-            sb.append(scan.nextLine() + "\n");
-        }
-        fr.close();
-    }
-
-    private void recordFile(String note) throws IOException{
-        FileWriter fw = new FileWriter("notebook.txt", true);
-        fw.write(note + "\n");
-        fw.close();
+    @Override
+    public void setNote(String text) {
+        String note = text;
     }
 }
