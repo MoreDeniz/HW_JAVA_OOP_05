4ff0da34c1ab8cbc0c63aadd68bdc01531588b4e
Mayya <msalosina@mail.ru>
2023-03-22 19:53:01 +0700

    ui.View

--- src/main/java/ui/View.java
+++ src/main/java/ui/View.java
@@ -1,2 +1,10 @@
-package ui;public interface View {
+package ui;
+
+import presenter.Presenter;
+
+
+public interface View {
+    void setPresenter(Presenter presenter);
+    void start() throws Exception;
+    void print(String text);
 }
